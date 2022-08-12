bc9c7658dd5c14c4392d26057fcc5507e7a71f0f
Daniel medina <danielmedina7778@gmail.com>
2022-08-12 11:36:39 -0500

    cambiando carpetas

---
+++ index.js
@@ -0,0 +1,13 @@
+
+document.addEventListener("click", e =>{
+
+  if(e.target.matches(".mostrar")){
+    const linksMostrar = document.querySelector(".panel")
+    linksMostrar.classList.toggle("is-active")
+  }
+  if(e.target.matches(".panel a") || e.target.matches("img") ){
+    document.querySelector(".panel").classList.remove("is-active")
+
+  }
+
+})
