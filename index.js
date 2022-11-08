3ad8961fc92018c2c4d79fb09c3645136e438c03
Daniel medina <danielmedina7778@gmail.com>
2022-11-07 20:36:07 -0500

    terminando cambios

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-
-document.addEventListener("click", e =>{
-
-  if(e.target.matches(".mostrar")){
-    const linksMostrar = document.querySelector(".panel")
-    linksMostrar.classList.toggle("is-active")
+document.addEventListener("click", (e) => {
+  if (e.target.matches(".mostrar")) {
+    const linksMostrar = document.querySelector(".panel");
+    linksMostrar.classList.toggle("is-active");
   }
-  if(e.target.matches(".panel a") || e.target.matches("img") ){
-    document.querySelector(".panel").classList.remove("is-active")
-
+  if (e.target.matches(".panel a") || e.target.matches("img")) {
+    document.querySelector(".panel").classList.remove("is-active");
   }
-
-})
+});
